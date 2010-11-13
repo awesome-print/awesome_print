49c50598139fcefb71b70921ca4da9ee6e046aaa
Mike Dvorkin <mike@dvorkin.net>
2010-11-13 09:42:50 -0800

    Fixed Rails 2.3.x console stack overflow issue

--- rails/init.rb
+++ rails/init.rb
@@ -1,1 +1,13 @@
-require File.join(File.dirname(__FILE__), "..", "init")
+# Copyright (c) 2010 Michael Dvorkin
+#
+# Awesome Print is freely distributable under the terms of MIT license.
+# See LICENSE file or http://www.opensource.org/licenses/mit-license.php
+#------------------------------------------------------------------------------
+#
+# Load awesome_print when installed as Rails 2.3.x plugin.
+#
+# NOTE: After Rails 2.3.x console loads awesome_print/lib/ap.rb it attempts
+# to load this file as well.  Make sure to check whether the awesome_print
+# is already loaded to avoid Ruby stack overflow when extending core classes.
+#
+require File.join(File.dirname(__FILE__), "..", "init") unless defined?(AwesomePrint)
