b0e47f11d23ba19ac67b2b628da506b1268dd8f4
Mike Dvorkin <mike@dvorkin.net>
2011-05-04 17:55:31 -0700

    Skip ActionView, ActiveRecord, and MongoMapper specs if the gems are not installed

--- spec/action_view_spec.rb
+++ spec/action_view_spec.rb
@@ -1,35 +1,40 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-require 'action_view'
-require 'ap/mixin/action_view'
+begin
+  require 'action_view'
+  require 'ap/mixin/action_view'
 
-describe "AwesomePrint ActionView extensions" do
-  before(:each) do
-    @view = ActionView::Base.new
-  end
+  describe "AwesomePrint ActionView extensions" do
+    before(:each) do
+      @view = ActionView::Base.new
+    end
     
-  it "should wrap ap output with <pre> tag" do
-    obj = 42
-    @view.ap(obj, :plain => true).should == '<pre class="debug_dump">42</pre>'
-  end
+    it "should wrap ap output with <pre> tag" do
+      obj = 42
+      @view.ap(obj, :plain => true).should == '<pre class="debug_dump">42</pre>'
+    end
 
-  it "should encode HTML entities" do
-    obj = "  &<hello>"
-    @view.ap(obj, :plain => true).should == '<pre class="debug_dump">&quot;  &amp;&lt;hello&gt;&quot;</pre>'
-  end
+    it "should encode HTML entities" do
+      obj = "  &<hello>"
+      @view.ap(obj, :plain => true).should == '<pre class="debug_dump">&quot;  &amp;&lt;hello&gt;&quot;</pre>'
+    end
 
-  it "should convert primary ANSI colors to HTML" do
-    obj = 42
-    [ :gray, :red, :green, :yellow, :blue, :purple, :cyan, :white ].each do |color|
-      @view.ap(obj, :color => { :fixnum => color }).should == %Q|<pre class="debug_dump"><font color="#{color}">42</font></pre>|
+    it "should convert primary ANSI colors to HTML" do
+      obj = 42
+      [ :gray, :red, :green, :yellow, :blue, :purple, :cyan, :white ].each do |color|
+        @view.ap(obj, :color => { :fixnum => color }).should == %Q|<pre class="debug_dump"><font color="#{color}">42</font></pre>|
+      end
+    end
+
+    it "should convert mixed ANSI colors to HTML" do
+      obj = 42
+      [ :grayish, :redish, :greenish, :yellowish, :blueish, :purpleish, :cyanish, :whiteish, :black, :pale ].zip(
+      [ :black, :darkred, :darkgreen, :brown, :navy, :darkmagenta, :darkcyan, :slategray, :black, :slategray ]) do |ansi, html|
+        @view.ap(obj, :color => { :fixnum => ansi }).should == %Q|<pre class="debug_dump"><font color="#{html}">42</font></pre>|
+      end
     end
   end
 
-  it "should convert mixed ANSI colors to HTML" do
-    obj = 42
-    [ :grayish, :redish, :greenish, :yellowish, :blueish, :purpleish, :cyanish, :whiteish, :black, :pale ].zip(
-    [ :black, :darkred, :darkgreen, :brown, :navy, :darkmagenta, :darkcyan, :slategray, :black, :slategray ]) do |ansi, html|
-      @view.ap(obj, :color => { :fixnum => ansi }).should == %Q|<pre class="debug_dump"><font color="#{html}">42</font></pre>|
-    end
-  end
+rescue LoadError
+  puts "Skipping ActionView specs..."
 end
--- spec/mongo_mapper_spec.rb
+++ spec/mongo_mapper_spec.rb
@@ -1,58 +1,63 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-require "mongo_mapper"
-require "ap/mixin/mongo_mapper"
+begin
+  require "mongo_mapper"
+  require "ap/mixin/mongo_mapper"
 
-if defined?(MongoMapper)
-  class User
-    include MongoMapper::Document
+  if defined?(MongoMapper)
+    class User
+      include MongoMapper::Document
 
-    key :first_name, String
-    key :last_name, String
-  end
-
-  describe "AwesomePrint/MongoMapper" do
-    before :each do
-      @ap = AwesomePrint.new(:plain => true)
+      key :first_name, String
+      key :last_name, String
     end
 
-    it "should print for a class instance" do
-      user = User.new(:first_name => "Al", :last_name => "Capone")
-      out = @ap.send(:awesome, user)
-      str = <<-EOS.strip
+    describe "AwesomePrint/MongoMapper" do
+      before :each do
+        @ap = AwesomePrint.new(:plain => true)
+      end
+
+      it "should print for a class instance" do
+        user = User.new(:first_name => "Al", :last_name => "Capone")
+        out = @ap.send(:awesome, user)
+        str = <<-EOS.strip
 #<User:0x01234567> {
            "_id" => BSON::ObjectId('4d9183739a546f6806000001'),
      "last_name" => "Capone",
     "first_name" => "Al"
 }
 EOS
-      out.gsub!(/'([\w]+){23}'/, "'4d9183739a546f6806000001'")
-      out.gsub!(/0x([a-f\d]+)/, "0x01234567")
-      out.should == str
-    end
+        out.gsub!(/'([\w]+){23}'/, "'4d9183739a546f6806000001'")
+        out.gsub!(/0x([a-f\d]+)/, "0x01234567")
+        out.should == str
+      end
 
-    it "should print for a class" do
-      @ap.send(:awesome, User).should == <<-EOS.strip
+      it "should print for a class" do
+        @ap.send(:awesome, User).should == <<-EOS.strip
 class User < Object {
            "_id" => :object_id,
      "last_name" => :string,
     "first_name" => :string
 }
 EOS
-    end
-
-    it "should print for a class when type is undefined" do
-      class Chamelion
-        include MongoMapper::Document
-        key :last_attribute
       end
 
-      @ap.send(:awesome, Chamelion).should == <<-EOS.strip
+      it "should print for a class when type is undefined" do
+        class Chamelion
+          include MongoMapper::Document
+          key :last_attribute
+        end
+
+        @ap.send(:awesome, Chamelion).should == <<-EOS.strip
 class Chamelion < Object {
                "_id" => :object_id,
     "last_attribute" => :undefined
 }
 EOS
+      end
     end
   end
+
+rescue LoadError
+  puts "Skipping MongoMapper specs..."
 end
