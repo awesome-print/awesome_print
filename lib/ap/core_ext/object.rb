894e35ebd90f79264dedb035d98c6a3f88bb51b0
Mike Dvorkin <mike@dvorkin.net>
2011-05-13 13:58:00 -0700

    Use anonymous method instead of method alias for Array, Class and Object extensions

--- lib/ap/core_ext/object.rb
+++ lib/ap/core_ext/object.rb
@@ -6,10 +6,10 @@
 class Object #:nodoc:
   # Remaining instance '_methods' are handled in core_ext/class.rb.
   %w(methods private_methods protected_methods public_methods singleton_methods).each do |name|
-    alias :"original_#{name}" :"#{name}"
+    original_method = instance_method(name)
 
     define_method name do |*args|
-      methods = self.__send__(:"original_#{name}", *args)
+      methods = original_method.bind(self).call(*args)
       methods.instance_variable_set('@__awesome_methods__', self) # Evil?!
       methods.sort!
     end
