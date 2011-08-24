620eb28117cf6497e456bd7718ec0fe23e27ffea
amd <amd@gurge.com>
2011-08-24 16:45:52 -0700

    don't forget to load it

--- lib/ap.rb
+++ lib/ap.rb
@@ -21,4 +21,5 @@ unless defined?(AwesomePrint)
   require File.dirname(__FILE__) + "/ap/mixin/active_record"  if defined?(ActiveRecord)  || (defined?(IRB) && ENV['RAILS_ENV'])
   require File.dirname(__FILE__) + "/ap/mixin/active_support" if defined?(ActiveSupport) || (defined?(IRB) && ENV['RAILS_ENV'])
   require File.dirname(__FILE__) + "/ap/mixin/mongo_mapper"   if defined?(MongoMapper)
+  require File.dirname(__FILE__) + "/ap/mixin/mongoid"        if defined?(Mongoid)  
 end
