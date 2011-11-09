2566e5c1b12a08721381201eaa19c94fa3521721
Mike Dvorkin <mike@dvorkin.net>
2011-11-08 22:07:27 -0800

    Use better namespace for ActionView plugin

--- lib/awesome_print/ext/action_view.rb
+++ lib/awesome_print/ext/action_view.rb
@@ -3,15 +3,16 @@
 # Awesome Print is freely distributable under the terms of MIT license.
 # See LICENSE file or http://www.opensource.org/licenses/mit-license.php
 #------------------------------------------------------------------------------
-module AwesomePrintActionView
+module AwesomePrint
+  module ActionView
 
-  # Use HTML colors and add default "debug_dump" class to the resulting HTML.
-  def ap_debug(object, options = {})
-    object.ai(options.merge(:html => true)).sub(/^<pre([\s>])/, '<pre class="debug_dump"\\1')
+    # Use HTML colors and add default "debug_dump" class to the resulting HTML.
+    def ap_debug(object, options = {})
+      object.ai(options.merge(:html => true)).sub(/^<pre([\s>])/, '<pre class="debug_dump"\\1')
+    end
+
+    alias_method :ap, :ap_debug
   end
-
-  alias_method :ap, :ap_debug
-
 end
 
-ActionView::Base.send(:include, AwesomePrintActionView) if defined?(ActionView)
+ActionView::Base.send(:include, AwesomePrint::ActionView) if defined?(ActionView)
