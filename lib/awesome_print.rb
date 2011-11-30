91ac8e080dd4437f3d6cd5b819f4cc70ba007402
Mike Dvorkin <mike@dvorkin.net>
2011-11-29 19:58:55 -0800

    Require Nokogiri plugin if the gem is present

--- lib/awesome_print.rb
+++ lib/awesome_print.rb
@@ -16,12 +16,15 @@ unless defined?(AwesomePrint)
   require File.dirname(__FILE__) + "/awesome_print/formatter"
   require File.dirname(__FILE__) + "/awesome_print/version"
   require File.dirname(__FILE__) + "/awesome_print/core_ext/logger" if defined?(Logger)
-  require File.dirname(__FILE__) + "/awesome_print/ext/action_view" if defined?(ActionView)
 
   # Load the following under normal circumstances as well as in Rails
   # console when required from ~/.irbrc.
   require File.dirname(__FILE__) + "/awesome_print/ext/active_record"  if defined?(ActiveRecord)  || (defined?(IRB) && ENV['RAILS_ENV'])
   require File.dirname(__FILE__) + "/awesome_print/ext/active_support" if defined?(ActiveSupport) || (defined?(IRB) && ENV['RAILS_ENV'])
+
+  # Load remaining extensions.
+  require File.dirname(__FILE__) + "/awesome_print/ext/action_view"    if defined?(ActionView)
   require File.dirname(__FILE__) + "/awesome_print/ext/mongo_mapper"   if defined?(MongoMapper)
   require File.dirname(__FILE__) + "/awesome_print/ext/mongoid"        if defined?(Mongoid)
+  require File.dirname(__FILE__) + "/awesome_print/ext/nokogiri"       if defined?(Nokogiri)
 end
