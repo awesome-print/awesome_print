a26b9028a9f3b44ccab557c1030effc9a0d43df0
Jonas Grimfelt <grimen@gmail.com>
2011-12-10 22:05:38 +0100

    Bugfix: "uninitialized constant ActionView::Base (NameError)". Checking namespace (e.g. ActionView) only is not reliable as ap requires the base  classes/modules (e.g. ActionView::Base).

--- lib/awesome_print.rb
+++ lib/awesome_print.rb
@@ -19,12 +19,12 @@ unless defined?(AwesomePrint)
 
   # Load the following under normal circumstances as well as in Rails
   # console when required from ~/.irbrc.
-  require File.dirname(__FILE__) + "/awesome_print/ext/active_record"  if defined?(ActiveRecord)  || (defined?(IRB) && ENV['RAILS_ENV'])
+  require File.dirname(__FILE__) + "/awesome_print/ext/active_record"  if defined?(ActiveRecord::Base) || (defined?(IRB) && ENV['RAILS_ENV'])
   require File.dirname(__FILE__) + "/awesome_print/ext/active_support" if defined?(ActiveSupport) || (defined?(IRB) && ENV['RAILS_ENV'])
 
   # Load remaining extensions.
   require File.dirname(__FILE__) + "/awesome_print/ext/action_view"    if defined?(ActionView::Base)
-  require File.dirname(__FILE__) + "/awesome_print/ext/mongo_mapper"   if defined?(MongoMapper)
-  require File.dirname(__FILE__) + "/awesome_print/ext/mongoid"        if defined?(Mongoid)
+  require File.dirname(__FILE__) + "/awesome_print/ext/mongo_mapper"   if defined?(MongoMapper::Document)
+  require File.dirname(__FILE__) + "/awesome_print/ext/mongoid"        if defined?(Mongoid::Document)
   require File.dirname(__FILE__) + "/awesome_print/ext/nokogiri"       if defined?(Nokogiri)
 end
