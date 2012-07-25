5cd2c46b1d2f80538faa492b4d7e3472ed16b922
Kamil K. Lemański <kamil.lemanski@gmail.com>
2012-07-25 22:54:00 +0300

    Update lib/awesome_print/ext/mongoid.rb
    quick fix for mongoid 3.x "NameError: uninitialized constant BSON"

--- lib/awesome_print/ext/mongoid.rb
+++ lib/awesome_print/ext/mongoid.rb
@@ -20,7 +20,7 @@ module AwesomePrint
           cast = :mongoid_class
         elsif object.class.ancestors.include?(::Mongoid::Document)
           cast = :mongoid_document
-        elsif object.is_a?(::BSON::ObjectId)
+        elsif defined?(::BSON) && object.is_a?(::BSON::ObjectId)
           cast = :mongoid_bson_id
         end
       end
