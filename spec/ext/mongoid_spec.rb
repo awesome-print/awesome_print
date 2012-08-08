2779249a4bd0bb0436eab1034e559211cbd19b1c
Nikolaj Nikolajsen <nn@peytz.dk>
2012-08-08 13:22:15 +0200

    Adding support for Mongoid 3 and Moped

--- spec/ext/mongoid_spec.rb
+++ spec/ext/mongoid_spec.rb
@@ -22,22 +22,37 @@ begin
       user = MongoUser.new :first_name => "Al", :last_name => "Capone"
       out = @ap.send :awesome, user
 
-      str = <<-EOS.strip
+      if defined?(::Moped)
+        str = <<-EOS.strip
+#<MongoUser:0x01234567> {
+           :_id => "424242424242424242424242",
+    :first_name => "Al",
+     :last_name => "Capone"
+}
+EOS
+      else
+        str = <<-EOS.strip
 #<MongoUser:0x01234567> {
            :_id => BSON::ObjectId('424242424242424242424242'),
     :first_name => "Al",
      :last_name => "Capone"
 }
 EOS
+      end
       out.gsub!(/0x([a-f\d]+)/, "0x01234567")
-      out.gsub!(/Id\('[^']+/, "Id('424242424242424242424242")
+      if defined?(::Moped)
+        out.gsub!(/:_id => \"[^"]+/, ":_id => \"424242424242424242424242")
+      else
+        out.gsub!(/Id\('[^']+/, "Id('424242424242424242424242")
+      end
       out.should == str
     end
 
     it "should print the class" do
+      moped_or_not = defined?(::Moped) ? 'moped/' : ''
       @ap.send(:awesome, MongoUser).should == <<-EOS.strip
 class MongoUser < Object {
-           :_id => :"bson/object_id",
+           :_id => :"#{moped_or_not}bson/object_id",
          :_type => :string,
     :first_name => :string,
      :last_name => :string
@@ -51,11 +66,13 @@ EOS
         field :last_attribute
       end
 
+      moped_or_not = defined?(::Moped) ? 'moped/' : ''
+      last_attribute = defined?(::BSON) ? '"mongoid/fields/serializable/object"' : 'object'
       @ap.send(:awesome, Chamelion).should == <<-EOS.strip
 class Chamelion < Object {
-               :_id => :"bson/object_id",
+               :_id => :"#{moped_or_not}bson/object_id",
              :_type => :string,
-    :last_attribute => :"mongoid/fields/serializable/object"
+    :last_attribute => :#{last_attribute}
 }
 EOS
     end
