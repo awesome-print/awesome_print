33790e63ff0bfd58fd820fc4d1d72d8ac4dbcdd6
Michael Dvorkin <mike@dvorkin.net>
2013-10-08 15:26:29 -0700

    Fixed ActiveRecord 4.0 specs

--- spec/ext/active_record_spec.rb
+++ spec/ext/active_record_spec.rb
@@ -118,7 +118,105 @@ EOS
 
           # ActiveRecord 3.1 and on.
           #--------------------------------------------------------------------------
-          if ActiveRecord::VERSION::STRING >= "3.1"
+          if ActiveRecord::VERSION::STRING >= "4.0"
+            str = <<-EOS.strip
+#<User:0x01234567
+    @_start_transaction_state = {},
+    @aggregation_cache = {},
+    @attributes_cache = {},
+    @columns_hash = {
+             "admin" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+            attr_accessor :coder = nil,
+            attr_accessor :primary = nil,
+            attr_reader :default = nil,
+            attr_reader :limit = nil,
+            attr_reader :name = "admin",
+            attr_reader :null = true,
+            attr_reader :precision = nil,
+            attr_reader :scale = nil,
+            attr_reader :sql_type = "boolean",
+            attr_reader :type = :boolean
+        >,
+        "created_at" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+            attr_accessor :coder = nil,
+            attr_accessor :primary = nil,
+            attr_reader :default = nil,
+            attr_reader :limit = nil,
+            attr_reader :name = "created_at",
+            attr_reader :null = true,
+            attr_reader :precision = nil,
+            attr_reader :scale = nil,
+            attr_reader :sql_type = "datetime",
+            attr_reader :type = :datetime
+        >,
+                "id" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+            attr_accessor :coder = nil,
+            attr_accessor :primary = nil,
+            attr_reader :default = nil,
+            attr_reader :limit = nil,
+            attr_reader :name = "id",
+            attr_reader :null = true,
+            attr_reader :precision = nil,
+            attr_reader :scale = nil,
+            attr_reader :sql_type = "integer",
+            attr_reader :type = :integer
+        >,
+              "name" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+            attr_accessor :coder = nil,
+            attr_accessor :primary = nil,
+            attr_reader :default = nil,
+            attr_reader :limit = nil,
+            attr_reader :name = "name",
+            attr_reader :null = true,
+            attr_reader :precision = nil,
+            attr_reader :scale = nil,
+            attr_reader :sql_type = "string",
+            attr_reader :type = :string
+        >,
+              "rank" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+            attr_accessor :coder = nil,
+            attr_accessor :primary = nil,
+            attr_reader :default = nil,
+            attr_reader :limit = nil,
+            attr_reader :name = "rank",
+            attr_reader :null = true,
+            attr_reader :precision = nil,
+            attr_reader :scale = nil,
+            attr_reader :sql_type = "integer",
+            attr_reader :type = :integer
+        >
+    },
+    @destroyed = false,
+    @marked_for_destruction = false,
+    @new_record = true,
+    @previously_changed = {},
+    @readonly = false,
+    @reflects_state = [
+        [0] false
+    ],
+    @transaction_state = nil,
+    @txn = nil,
+    attr_accessor :attributes = {
+             "admin" => false,
+        "created_at" => "1992-10-10 12:30:00",
+                "id" => nil,
+                 :id => nil,
+              "name" => "Diana",
+              "rank" => 1
+    },
+    attr_accessor :destroyed_by_association = nil,
+    attr_reader :association_cache = {},
+    attr_reader :changed_attributes = {
+             "admin" => nil,
+        "created_at" => nil,
+              "name" => nil,
+              "rank" => nil
+    }
+>
+EOS
+          # ActiveRecord 3.1 and on.
+          #--------------------------------------------------------------------------
+          elsif ActiveRecord::VERSION::STRING >= "3.1"
             str = <<-EOS.strip
 #<User:0x01234567
     @aggregation_cache = {},
@@ -179,9 +277,202 @@ EOS
         it "display multiple records" do
           out = @ap.send(:awesome, [ @diana, @laura ])
 
-          # ActiveRecord 3.1 and on.
+          # ActiveRecord 4.0 and on.
           #--------------------------------------------------------------------------
           if ActiveRecord::VERSION::STRING >= "3.1"
+            str = <<-EOS.strip
+[
+    [0] #<User:0x01234567
+        @_start_transaction_state = {},
+        @aggregation_cache = {},
+        @attributes_cache = {},
+        @columns_hash = {
+                 "admin" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+                attr_accessor :coder = nil,
+                attr_accessor :primary = nil,
+                attr_reader :default = nil,
+                attr_reader :limit = nil,
+                attr_reader :name = "admin",
+                attr_reader :null = true,
+                attr_reader :precision = nil,
+                attr_reader :scale = nil,
+                attr_reader :sql_type = "boolean",
+                attr_reader :type = :boolean
+            >,
+            "created_at" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+                attr_accessor :coder = nil,
+                attr_accessor :primary = nil,
+                attr_reader :default = nil,
+                attr_reader :limit = nil,
+                attr_reader :name = "created_at",
+                attr_reader :null = true,
+                attr_reader :precision = nil,
+                attr_reader :scale = nil,
+                attr_reader :sql_type = "datetime",
+                attr_reader :type = :datetime
+            >,
+                    "id" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+                attr_accessor :coder = nil,
+                attr_accessor :primary = nil,
+                attr_reader :default = nil,
+                attr_reader :limit = nil,
+                attr_reader :name = "id",
+                attr_reader :null = true,
+                attr_reader :precision = nil,
+                attr_reader :scale = nil,
+                attr_reader :sql_type = "integer",
+                attr_reader :type = :integer
+            >,
+                  "name" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+                attr_accessor :coder = nil,
+                attr_accessor :primary = nil,
+                attr_reader :default = nil,
+                attr_reader :limit = nil,
+                attr_reader :name = "name",
+                attr_reader :null = true,
+                attr_reader :precision = nil,
+                attr_reader :scale = nil,
+                attr_reader :sql_type = "string",
+                attr_reader :type = :string
+            >,
+                  "rank" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+                attr_accessor :coder = nil,
+                attr_accessor :primary = nil,
+                attr_reader :default = nil,
+                attr_reader :limit = nil,
+                attr_reader :name = "rank",
+                attr_reader :null = true,
+                attr_reader :precision = nil,
+                attr_reader :scale = nil,
+                attr_reader :sql_type = "integer",
+                attr_reader :type = :integer
+            >
+        },
+        @destroyed = false,
+        @marked_for_destruction = false,
+        @new_record = true,
+        @previously_changed = {},
+        @readonly = false,
+        @reflects_state = [
+            [0] false
+        ],
+        @transaction_state = nil,
+        @txn = nil,
+        attr_accessor :attributes = {
+                 "admin" => false,
+            "created_at" => "1992-10-10 12:30:00",
+                    "id" => nil,
+                     :id => nil,
+                  "name" => "Diana",
+                  "rank" => 1
+        },
+        attr_accessor :destroyed_by_association = nil,
+        attr_reader :association_cache = {},
+        attr_reader :changed_attributes = {
+                 "admin" => nil,
+            "created_at" => nil,
+                  "name" => nil,
+                  "rank" => nil
+        }
+    >,
+    [1] #<User:0x01234567
+        @_start_transaction_state = {},
+        @aggregation_cache = {},
+        @attributes_cache = {},
+        @columns_hash = {
+                 "admin" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+                attr_accessor :coder = nil,
+                attr_accessor :primary = nil,
+                attr_reader :default = nil,
+                attr_reader :limit = nil,
+                attr_reader :name = "admin",
+                attr_reader :null = true,
+                attr_reader :precision = nil,
+                attr_reader :scale = nil,
+                attr_reader :sql_type = "boolean",
+                attr_reader :type = :boolean
+            >,
+            "created_at" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+                attr_accessor :coder = nil,
+                attr_accessor :primary = nil,
+                attr_reader :default = nil,
+                attr_reader :limit = nil,
+                attr_reader :name = "created_at",
+                attr_reader :null = true,
+                attr_reader :precision = nil,
+                attr_reader :scale = nil,
+                attr_reader :sql_type = "datetime",
+                attr_reader :type = :datetime
+            >,
+                    "id" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+                attr_accessor :coder = nil,
+                attr_accessor :primary = nil,
+                attr_reader :default = nil,
+                attr_reader :limit = nil,
+                attr_reader :name = "id",
+                attr_reader :null = true,
+                attr_reader :precision = nil,
+                attr_reader :scale = nil,
+                attr_reader :sql_type = "integer",
+                attr_reader :type = :integer
+            >,
+                  "name" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+                attr_accessor :coder = nil,
+                attr_accessor :primary = nil,
+                attr_reader :default = nil,
+                attr_reader :limit = nil,
+                attr_reader :name = "name",
+                attr_reader :null = true,
+                attr_reader :precision = nil,
+                attr_reader :scale = nil,
+                attr_reader :sql_type = "string",
+                attr_reader :type = :string
+            >,
+                  "rank" => #<ActiveRecord::ConnectionAdapters::Column:0x01234567
+                attr_accessor :coder = nil,
+                attr_accessor :primary = nil,
+                attr_reader :default = nil,
+                attr_reader :limit = nil,
+                attr_reader :name = "rank",
+                attr_reader :null = true,
+                attr_reader :precision = nil,
+                attr_reader :scale = nil,
+                attr_reader :sql_type = "integer",
+                attr_reader :type = :integer
+            >
+        },
+        @destroyed = false,
+        @marked_for_destruction = false,
+        @new_record = true,
+        @previously_changed = {},
+        @readonly = false,
+        @reflects_state = [
+            [0] false
+        ],
+        @transaction_state = nil,
+        @txn = nil,
+        attr_accessor :attributes = {
+                 "admin" => true,
+            "created_at" => "2003-05-26 14:15:00",
+                    "id" => nil,
+                     :id => nil,
+                  "name" => "Laura",
+                  "rank" => 2
+        },
+        attr_accessor :destroyed_by_association = nil,
+        attr_reader :association_cache = {},
+        attr_reader :changed_attributes = {
+                 "admin" => nil,
+            "created_at" => nil,
+                  "name" => nil,
+                  "rank" => nil
+        }
+    >
+]
+EOS
+          # ActiveRecord 3.1 and on.
+          #--------------------------------------------------------------------------
+          elsif ActiveRecord::VERSION::STRING >= "3.1"
             str = <<-EOS.strip
 [
     [0] #<User:0x01234567
