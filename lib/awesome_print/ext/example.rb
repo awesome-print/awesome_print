525845bafe231dbf3b110e5e10799ad80ca8a9ec
Michael Dvorkin <mike@dvorkin.net>
2013-10-09 22:55:47 -0700

    Added radix plugin example

---
+++ lib/awesome_print/ext/example.rb
@@ -0,0 +1,16 @@
+module AwesomePrint
+  module Radix
+
+    def cast(object, type)
+      # puts "example#cast(#{object.inspect}, #{type.inspect})"
+      :radix if object.is_a?(Fixnum)
+    end
+
+    private
+
+    def awesome_radix(object)
+      # puts "example#awesome_radix(#{object.inspect})"
+      "#{object} (dec) 0#{object.to_s(8)} (oct) 0x#{object.to_s(16).upcase}"
+    end
+  end
+end
