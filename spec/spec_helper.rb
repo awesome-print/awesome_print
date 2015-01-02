d8c45625992fb28abf0948dcc0fdbc797c31ef1c
Mauro George <maurogot@gmail.com>
2015-01-02 15:27:43 -0200

    Remove 1.8.6 old monkey patch

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,36 +37,3 @@ def capture!
 ensure
   $stdout = standard
 end
-
-# The following is needed for the Infinity Test. It runs tests as subprocesses,
-# which sets STDOUT.tty? to false and would otherwise prematurely disallow colors.
-### AwesomePrint.force_colors!
-
-# Ruby 1.8.6 only: define missing String methods that are needed for the specs to pass.
-if RUBY_VERSION < '1.8.7'
-  class String
-    def shellescape # Taken from Ruby 1.9.2 standard library, see lib/shellwords.rb.
-      return "''" if self.empty?
-      str = self.dup
-      str.gsub!(/([^A-Za-z0-9_\-.,:\/@\n])/n, "\\\\\\1")
-      str.gsub!(/\n/, "'\n'")
-      str
-    end
-
-    def start_with?(*prefixes)
-      prefixes.each do |prefix|
-        prefix = prefix.to_s
-        return true if prefix == self[0, prefix.size]
-      end
-      false
-    end
-
-    def end_with?(*suffixes)
-      suffixes.each do |suffix|
-        suffix = suffix.to_s
-        return true if suffix == self[-suffix.size, suffix.size]
-      end
-      false
-    end
-  end
-end
