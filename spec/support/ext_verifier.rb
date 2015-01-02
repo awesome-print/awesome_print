0f501257d8e0f7c389cb4d087b5acc25a67504ce
Mauro George <maurogot@gmail.com>
2015-01-02 15:08:25 -0200

    Remove :: when check for defined constants

--- spec/support/ext_verifier.rb
+++ spec/support/ext_verifier.rb
@@ -11,22 +11,22 @@ module ExtVerifier
   module_function :require_dependencies!
 
   def has_rails?
-    defined?(::Rails)
+    defined?(Rails)
   end
   module_function :has_rails?
 
   def has_mongoid?
-    defined?(::Mongoid)
+    defined?(Mongoid)
   end
   module_function :has_mongoid?
 
   def has_mongo_mapper?
-    defined?(::MongoMapper)
+    defined?(MongoMapper)
   end
   module_function :has_mongo_mapper?
 
   def has_ripple?
-    defined?(::Ripple)
+    defined?(Ripple)
   end
   module_function :has_ripple?
 end
