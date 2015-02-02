4ee2ade5f72677e2afd92033e6f0d4c244a8e43d
Elliot Shank <github@galumph.com>
2015-02-02 11:15:23 -0600

    Be careful about invoking #to_hash on arbitrary objects.

--- lib/awesome_print/formatter.rb
+++ lib/awesome_print/formatter.rb
@@ -62,9 +62,9 @@ module AwesomePrint
     #------------------------------------------------------------------------------
     def awesome_self(object, type)
       if @options[:raw] && object.instance_variables.any?
-        awesome_object(object)
-      elsif object.respond_to?(:to_hash)
-        awesome_hash(object.to_hash)
+        return awesome_object(object)
+      elsif hash = convert_to_hash(object)
+        awesome_hash(hash)
       else
         colorize(object.inspect.to_s, type)
       end
@@ -318,6 +318,26 @@ module AwesomePrint
 
     # Utility methods.
     #------------------------------------------------------------------------------
+    def convert_to_hash(object)
+      if not object.respond_to?(:to_hash)
+        return nil
+      end
+      if object.method(:to_hash).arity != 0
+        return nil
+      end
+
+      hash = object.to_hash
+      if not hash
+        return nil
+      end
+
+      if not hash.respond_to?(:keys) or not hash.respond_to?('[]')
+        return nil
+      end
+
+      return hash
+    end
+
     def align(value, width)
       if @options[:multiline]
         if @options[:indent] > 0
