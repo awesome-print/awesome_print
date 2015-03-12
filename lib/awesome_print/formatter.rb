2426de2899a267f1ffb7bece47297a50bfe55ede
Mauro George <maurogot@gmail.com>
2015-03-12 19:52:22 -0300

    Create a AwesomePrint::Formatters to include all formatters

--- lib/awesome_print/formatter.rb
+++ lib/awesome_print/formatter.rb
@@ -1,20 +1,6 @@
 autoload :CGI, 'cgi'
 require 'shellwords'
-
-require 'awesome_print/formatters/base'
-require 'awesome_print/formatters/array'
-require 'awesome_print/formatters/hash'
-require 'awesome_print/formatters/object'
-require 'awesome_print/formatters/set'
-require 'awesome_print/formatters/struct'
-require 'awesome_print/formatters/class'
-require 'awesome_print/formatters/file'
-require 'awesome_print/formatters/dir'
-require 'awesome_print/formatters/big_decimal'
-require 'awesome_print/formatters/rational'
-require 'awesome_print/formatters/method'
-require 'awesome_print/formatters/active_record_instance'
-require 'awesome_print/formatters/active_record_class'
+require 'awesome_print/formatters'
 
 module AwesomePrint
   class Formatter
---
+++ lib/awesome_print/formatters.rb
@@ -0,0 +1,18 @@
+module AwesomePrint
+  module Formatters
+    require 'awesome_print/formatters/base'
+    require 'awesome_print/formatters/array'
+    require 'awesome_print/formatters/hash'
+    require 'awesome_print/formatters/object'
+    require 'awesome_print/formatters/set'
+    require 'awesome_print/formatters/struct'
+    require 'awesome_print/formatters/class'
+    require 'awesome_print/formatters/file'
+    require 'awesome_print/formatters/dir'
+    require 'awesome_print/formatters/big_decimal'
+    require 'awesome_print/formatters/rational'
+    require 'awesome_print/formatters/method'
+    require 'awesome_print/formatters/active_record_instance'
+    require 'awesome_print/formatters/active_record_class'
+  end
+end
