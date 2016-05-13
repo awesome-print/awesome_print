0a93e5bc939cab096b45b3285c7912e2bdda098f
Waldyr de Souza <waldyr.ar@gmail.com>
2016-05-13 00:22:11 -0300

    Add file formatter

---
+++ lib/awesome_print/formatters/file_formatter.rb
@@ -0,0 +1,23 @@
+require_relative 'base_formatter'
+require "shellwords"
+
+module AwesomePrint
+  module Formatters
+    class FileFormatter < BaseFormatter
+
+      attr_reader :file, :inspector, :options, :indentation
+
+      def initialize(file, inspector)
+        @file = file
+        @inspector = inspector
+        @options = inspector.options
+        @indentation = @options[:indent].abs
+      end
+
+      def format
+        ls = File.directory?(file) ? `ls -adlF #{file.path.shellescape}` : `ls -alF #{file.path.shellescape}`
+        colorize(ls.empty? ? file.inspect : "#{file.inspect}\n#{ls.chop}", :file)
+      end
+    end
+  end
+end
