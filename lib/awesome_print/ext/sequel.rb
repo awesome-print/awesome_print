d3c41c9c0f2098b87cd269f5f52e414fc3639f38
Olle Jonsson <olle.jonsson@gmail.com>
2016-12-07 14:30:42 +0100

    Sequel: avoid shadowing outer variable

--- lib/awesome_print/ext/sequel.rb
+++ lib/awesome_print/ext/sequel.rb
@@ -45,8 +45,8 @@ module AwesomePrint
     # Format Sequel Model class.
     #------------------------------------------------------------------------------
     def awesome_sequel_model_class(object)
-      data = object.db_schema.inject({}) { |h, (name, data)| h.merge(name => data[:db_type]) }
-      "class #{object} < #{object.superclass} " << awesome_hash(data)
+      result = object.db_schema.inject({}) { |h, (name, data)| h.merge(name => data[:db_type]) }
+      "class #{object} < #{object.superclass} " << awesome_hash(result)
     end
   end
 
