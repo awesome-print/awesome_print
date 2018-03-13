2f0295b9ef495438c92ac3fc6bfdc7b6b0a22150
Patrik Wenger <patrik.wenger@mindclue.ch>
2018-03-13 16:01:03 +0100

    cache formatters during lifetime of one Inspector

--- lib/awesome_print/formatter.rb
+++ lib/awesome_print/formatter.rb
@@ -69,40 +69,49 @@ module AwesomePrint
     end
 
     def awesome_array(a)
-      Formatters::ArrayFormatter.new(a, @inspector).format
+      @array_formatter ||= Formatters::ArrayFormatter.new(:__placeholder,  @inspector)
+      @array_formatter.format_object(a)
     end
 
     def awesome_set(s)
-      Formatters::ArrayFormatter.new(s.to_a, @inspector).format
+      @array_formatter ||= Formatters::ArrayFormatter.new(:__placeholder, @inspector)
+      @array_formatter.format_object(s.to_a)
     end
 
     def awesome_hash(h)
-      Formatters::HashFormatter.new(h, @inspector).format
+      @hash_formatter ||= Formatters::HashFormatter.new(:__placeholder, @inspector)
+      @hash_formatter.format_object(h)
     end
 
     def awesome_object(o)
-      Formatters::ObjectFormatter.new(o, @inspector).format
+      @object_formatter ||= Formatters::ObjectFormatter.new(:__placeholder, @inspector)
+      @object_formatter.format_object(o)
     end
 
     def awesome_struct(s)
-      Formatters::StructFormatter.new(s, @inspector).format
+      @struct_formatter ||= Formatters::StructFormatter.new(:__placeholder, @inspector)
+      @struct_formatter.format_object(s)
     end
 
     def awesome_method(m)
-      Formatters::MethodFormatter.new(m, @inspector).format
+      @method_formatter ||= Formatters::MethodFormatter.new(:__placeholder, @inspector)
+      @method_formatter.format_object(m)
     end
     alias :awesome_unboundmethod :awesome_method
 
     def awesome_class(c)
-      Formatters::ClassFormatter.new(c, @inspector).format
+      @class_formatter ||= Formatters::ClassFormatter.new(:__placeholder, @inspector)
+      @class_formatter.format_object(c)
     end
 
     def awesome_file(f)
-      Formatters::FileFormatter.new(f, @inspector).format
+      @file_formatter ||= Formatters::FileFormatter.new(:__placeholder, @inspector)
+      @file_formatter.format_object(f)
     end
 
     def awesome_dir(d)
-      Formatters::DirFormatter.new(d, @inspector).format
+      @dir_formatter ||= Formatters::DirFormatter.new(:__placeholder, @inspector)
+      @dir_formatter.format_object(d)
     end
 
     # Utility methods.
