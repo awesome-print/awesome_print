5eb57ad5553bd6da7c08b725d344fa1a7078493a
James Cox <james@imaj.es>
2019-01-23 14:17:07 -0500

    Remove no longer required active record stubs

--- spec/support/rails_versions.rb
+++ spec/support/rails_versions.rb
@@ -14,29 +14,9 @@ module RailsVersions
   alias_method :activerecord_5_1?, :rails_5_1?
 
   def rails_5_0?
-    Gem::Requirement.new('~> 5.0.0.racecar1').satisfied_by?(rails_version)
+    Gem::Requirement.new('~> 5.0.0').satisfied_by?(rails_version)
   end
   alias_method :activerecord_5_0?, :rails_5_0?
-
-  def rails_4_2?
-    Gem::Requirement.new('~> 4.2.0').satisfied_by?(rails_version)
-  end
-  alias_method :activerecord_4_2?, :rails_4_2?
-
-  def rails_4_1?
-    Gem::Requirement.new('~> 4.1.0').satisfied_by?(rails_version)
-  end
-  alias_method :activerecord_4_1?, :rails_4_1?
-
-  def rails_4_0?
-    Gem::Requirement.new('~> 4.0.0').satisfied_by?(rails_version)
-  end
-  alias_method :activerecord_4_0?, :rails_4_0?
-
-  def rails_3_2?
-    Gem::Requirement.new('~> 3.2.0').satisfied_by?(rails_version)
-  end
-  alias_method :activerecord_3_2?, :rails_3_2?
 end
 
 RSpec.configure do |config|
